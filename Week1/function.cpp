24bc1c6ba2df51c4f2d49d149342d39e219ba86f
Enamul Haque Alvi <138120822+enamulalvi@users.noreply.github.com>
2024-06-23 01:10:59 +0600

    Add files via upload
    Welcome to the first week of our Data Structures course! We are diving into the fundamentals of programming with a focus on arrays in C++. This week, you will:
    
    Understand the Basics of Arrays: Learn what arrays are, how they store data, and why they are essential in programming.
    Declaration and Initialization: Get hands-on practice with declaring and initializing arrays in C++.
    Array Operations: Explore various operations such as accessing, modifying, and iterating through array elements.
    Memory Management: Gain insights into how arrays are managed in memory, including the concepts of static and dynamic arrays.
    Practical Applications: Implement simple programs that utilize arrays to solve common problems, enhancing your problem-solving skills and coding proficiency.

---
+++ Week1/function.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+void adding()
+{
+     int poss,num,i,temp;
+    cout << "Enter possition number & number "<<endl;
+    cin >> poss;
+    cin >> num;
+    cout << a[3];
+
+}
+void droping()
+{
+
+}
+
+int main ()
+{
+   // int poss,num,i,temp;
+   int temp;
+    int a[100]={11,12,13,14,15};
+
+    cout << "To add number press 1 and to drop number press 2 "<< endl;
+    cin >> temp;
+    if(temp==1)
+    {
+        adding(int a[5]);
+    }
+    else if(temp==2)
+    {
+        droping();
+    }
+
+
+}
