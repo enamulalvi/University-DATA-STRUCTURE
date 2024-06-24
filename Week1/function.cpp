3cff7d6bbbf283dfbcf76a82fa64df9b947d03b4
Enamul Haque Alvi <138120822+enamulalvi@users.noreply.github.com>
2024-06-24 15:48:06 +0600

    Update function.cpp

--- Week1/function.cpp
+++ Week1/function.cpp
@@ -1,61 +1,74 @@
 #include <iostream>
 using namespace std;
 
-void adding(int a[], int size) {
+void adding(int a[], int size)
+{
     int pos, num;
     cout << "Enter position number & number " << endl;
     cin >> pos;
     cin >> num;
 
-    if (pos < 0 || pos > size) {
+    if (pos < 0 || pos > size)
+    {
         cout << "Invalid position!" << endl;
         return;
     }
 
-    for (int i = size; i > pos; i--) {
+    for (int i = size; i > pos; i--)
+    {
         a[i] = a[i-1];
     }
     a[pos] = num;
 
-    for (int i = 0; i <= size; i++) {
+    for (int i = 0; i <= size; i++)
+    {
         cout << a[i] << " ";
     }
     cout << endl;
 }
 
-void droping(int a[], int size, int pos) {
-    if (pos < 0 || pos >= size) {
+void droping(int a[], int size, int pos)
+{
+    if (pos < 0 || pos >= size)
+    {
         cout << "Invalid position!" << endl;
         return;
     }
 
 
-    for (int i = pos; i < size - 1; i++) {
+    for (int i = pos; i < size - 1; i++)
+    {
         a[i] = a[i+1];
     }
     a[size - 1] = 0;
 
-    for (int i = 0; i < size - 1; i++) {
+    for (int i = 0; i < size - 1; i++)
+    {
         cout << a[i] << " ";
     }
     cout << endl;
 }
 
-int main() {
+int main()
+{
     int temp;
     int a[100] = {11, 12, 13, 14, 15};
     int size = 5;
 
     cout << "To add number press 1 and to drop number press 2 " << endl;
     cin >> temp;
-    if (temp == 1) {
+    if (temp == 1)
+    {
         adding(a, size);
-    } else if (temp == 2) {
+    } else if (temp == 2)
+    {
         int pos;
         cout << "Enter position number to drop: ";
         cin >> pos;
         droping(a, size, pos);
-    } else {
+    }
+    else
+    {
         cout << "Invalid choice!" << endl;
     }
 
