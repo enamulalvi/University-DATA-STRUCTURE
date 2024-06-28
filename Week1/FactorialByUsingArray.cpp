f71c0a76cc5b6178fb0151b7f0a191b667445761
Enamul Haque Alvi <138120822+enamulalvi@users.noreply.github.com>
2024-06-29 01:40:39 +0600

    Update FactorialByUsingArray.cpp

--- Week1/FactorialByUsingArray.cpp
+++ Week1/FactorialByUsingArray.cpp
@@ -1,19 +1,3 @@
-/*#include <iostream>
-using namespace std;
-int main()
-{
-    int n,sum=0;
-    cout << " enter the number ";
-    cin >> n;
-    int arr[300];
-    for(int i = 0; i<=n;i++)
-    {
-        cin>> arr[i];
-        sum = sum + arr[i];
-    }
-    cout << "sum "<< sum;
-}
-*/
 #include <iostream>
 using namespace std;
 int main()
